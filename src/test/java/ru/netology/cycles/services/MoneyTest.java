10793b95d33488aa9bfbfd9efda3705929564065
wbposha <135736179+wbposha@users.noreply.github.com>
2023-08-04 02:28:29 +0400

    Update MoneyTest.java

--- src/test/java/ru/netology/cycles/services/MoneyTest.java
+++ src/test/java/ru/netology/cycles/services/MoneyTest.java
@@ -11,7 +11,7 @@ public class MoneyTest{
         int income = 10000;
         int expenses = 3000;
         int threshold = 20000;
-        int expected = 2;
+        int expected = 9;
 
         int actual = service.calculate(income, expenses, threshold);
 
@@ -26,7 +26,7 @@ public class MoneyTest{
         int income = 100000;
         int expenses = 60000;
         int threshold = 150000;
-        int expected = 3;
+        int expected = 7;
 
         int actual = service.calculate(income, expenses, threshold);
 
